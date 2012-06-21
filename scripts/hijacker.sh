0c5b8bcf8e1d14f2f100a19d68e5afac64cb0939
Sean Donovan <swordrune10@gmail.com>
2012-06-20 23:16:40 -0500

    hijacker.sh: remove system/extras/su during hijack

--- scripts/hijacker.sh
+++ scripts/hijacker.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 cd $ANDROID_BUILD_TOP/build/target/product/security
-if ( ! find superuser.pk8 &> /dev/null);then
+if ( ! find superuser.pk8 &> /dev/null); then
 	wget https://github.com/ChainsDD/platform_build/raw/master/target/product/security/superuser.pk8
 	wget https://github.com/ChainsDD/platform_build/raw/master/target/product/security/superuser.x509.pem
 fi
 
+rm -rf $ANDROID_BUILD_TOP/system/extras/su
+
 export AOSP_BUILD=$(echo $TARGET_PRODUCT | sed -e 's/^full_//g')
 
 cd $ANDROID_BUILD_TOP/device/*/$AOSP_BUILD
-if ( ! grep -q "overrider.mk" $TARGET_PRODUCT.mk );then
+if ( ! grep -q "overrider.mk" $TARGET_PRODUCT.mk ); then
 	sed -i -e '/full_base.*.mk/ i \
 $(call inherit-product, vendor/keyblade/makefiles/overrider.mk)' \
 	$TARGET_PRODUCT.mk
