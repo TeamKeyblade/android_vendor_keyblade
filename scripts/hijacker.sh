9bbbb1250c216e94ee60727bc587a595fdd92cbb
swordrune10 <swordrune10@gmail.com>
2013-03-06 14:10:07 -0700

    remove obsolete superuser additions

--- scripts/hijacker.sh
+++ scripts/hijacker.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-cd $ANDROID_BUILD_TOP/build/target/product/security
-if ( ! find superuser.pk8 &> /dev/null); then
-	wget https://github.com/ChainsDD/platform_build/raw/master/target/product/security/superuser.pk8
-	wget https://github.com/ChainsDD/platform_build/raw/master/target/product/security/superuser.x509.pem
-fi
-
-rm -rf $ANDROID_BUILD_TOP/system/extras/su
 
 export AOSP_BUILD=$(echo $TARGET_PRODUCT | sed -e 's/^full_//g')
 
