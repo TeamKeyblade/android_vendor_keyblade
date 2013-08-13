c35d6a44bd3f53a5254dd737c3256a35e77e4c19
Sean Donovan <swordrune10@gmail.com>
2013-08-13 14:32:25 -0700

    update hijacker.sh to work with aosp/raosp 4.3

--- scripts/hijacker.sh
+++ scripts/hijacker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export AOSP_BUILD=$(echo $TARGET_PRODUCT | sed -e 's/^full_//g')
+export AOSP_BUILD=$(echo $TARGET_PRODUCT | sed -e 's/^full_//g' | sed -e 's/^aosp_//g')
 
 cd $ANDROID_BUILD_TOP/device/*/$AOSP_BUILD
-if ( ! grep -q "overrider.mk" $TARGET_PRODUCT.mk ); then
-	sed -i -e '/full_base.*.mk/ i \
+if ( ! grep -q "overrider.mk" full_$AOSP_BUILD.mk ); then
+	sed -i -e '/aosp_base.*.mk/ i \
 $(call inherit-product, vendor/keyblade/makefiles/overrider.mk)' \
 	$TARGET_PRODUCT.mk
 fi
