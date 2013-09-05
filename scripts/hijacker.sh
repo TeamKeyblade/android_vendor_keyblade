9ab010be25e0ee32f0ec65185c79fc115cc0623f
Sean Donovan <swordrune10@gmail.com>
2013-09-04 22:24:53 -0700

    fix derp with if

--- scripts/hijacker.sh
+++ scripts/hijacker.sh
@@ -2,6 +2,6 @@
 export AOSP_BUILD=$(echo $TARGET_PRODUCT | sed -e 's/^full_//g' | sed -e 's/^aosp_//g')
 
 cd $ANDROID_BUILD_TOP/device/*/$AOSP_BUILD
-if ( ! grep -q "overrider.mk" device.mk ); then
+if ( ! grep -q "keyblade.mk" device.mk ); then
 	echo '$(call inherit-product, vendor/keyblade/makefiles/keyblade.mk)' >> device.mk
 fi
