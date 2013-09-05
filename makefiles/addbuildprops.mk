517f91a8e5ecf77196f2b0e52d7216d2dca67e56
Sean Donovan <swordrune10@gmail.com>
2013-09-04 22:49:00 -0700

    use PRODUCT_DEVICE in modversion instead

--- makefiles/addbuildprops.mk
+++ makefiles/addbuildprops.mk
@@ -1,7 +1,7 @@
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.rommanager.developerid=teamkeyblade
 
-MOD_VERSION := raosp-jb-$(shell date +%Y%m%d)-$(AOSP_BUILD)
+MOD_VERSION := raosp-jb-$(shell date +%Y%m%d)-$(PRODUCT_DEVICE)
 
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.modversion=$(MOD_VERSION)
