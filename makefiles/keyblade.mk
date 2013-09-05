2f895ae77f52ba354a57cfa189cf019cc1f9fbca
Sean Donovan <swordrune10@gmail.com>
2013-09-04 17:23:14 -0700

    keyblade.mk: remove all the unnecessary shinanigans

--- makefiles/keyblade.mk
+++ makefiles/keyblade.mk
@@ -4,18 +4,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
 MOD_VERSION := raosp-jb-$(shell date +%Y%m%d)-$(AOSP_BUILD)
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    keyguard.no_require_sim=true \
-    ro.com.android.dataroaming=false \
-    ro.com.android.dateformat=MM-dd-yyyy \
     ro.modversion=$(MOD_VERSION) \
-    ro.config.ringtone=CASSIOPEIA.ogg \
-    ro.config.notification_sound=Fluorine.ogg \
-    ro.config.alarm_alert=Hassium.ogg
-
-SOUNDS_PATH:= frameworks/base/data/sounds
-
-PRODUCT_COPY_FILES += \
-	$(SOUNDS_PATH)/alarms/ogg/Hassium.ogg:system/media/audio/alarms/Hassium.ogg
 
 SUPERUSER_PACKAGE := com.teamkeyblade.superuser
 
