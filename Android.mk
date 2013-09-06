58cfc7487ac8235061d4fc7cd7c1d59aee4f55aa
Sean Donovan <swordrune10@gmail.com>
2013-09-05 18:23:20 -0700

    keyblade.mk: figure out its own variable for AOSP_BUILD
    also delete obsolete files because of this

--- Android.mk
+++
@@ -1,4 +0,0 @@
-# Written by Sean Donovan (swordrune10)
-LOCAL_PATH := $(call my-dir)
-
-$(shell ($(LOCAL_PATH)/scripts/variablemakir))
--- makefiles/keyblade.mk
+++ makefiles/keyblade.mk
@@ -4,6 +4,7 @@ PRODUCT_PACKAGES += \
     Superuser \
     su
 
+AOSP_BUILD := $(shell echo $TARGET_PRODUCT | sed -e 's/^full_//g' | sed -e 's/^aosp_//g')
 MOD_VERSION := raosp-jb-$(shell date +%Y%m%d)-$(AOSP_BUILD)
 
 ADDITIONAL_BUILD_PROPERTIES += \
